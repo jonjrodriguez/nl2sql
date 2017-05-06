15a169ebaea38d370d46896040a48c7fb690dc2b
hab278 <hab278@nyu.edu>
2017-05-05 20:55:13 -0400

    Create a SelectNode as a root node

---
+++ sql/SelectNode.py
@@ -0,0 +1,27 @@
+from SQLNodeMultiChild import SQLNodeMultiChild
+from SQLNodeType import SQLNodeType
+
+class SelectNode(SQLNodeMultiChild):
+    """
+    Usually the root node, this is a node of type select. Start with
+    this node, and add children nodes to it (one at a time).
+
+    root = SelectNode()
+    attribute_node = AttributeNode()
+    table_node = TableNode("students")
+
+    attribute_node.add_child(table_node)
+    root.add_child(attribute_node)
+
+    SQLTree.get_sql(root)
+
+    => SELECT * FROM 'students';
+
+    """
+    def __init__(self, label = "SELECT", child = None):
+        SQLNodeMultiChild.__init__(self, SQLNodeType.SELECT_NODE, label)
+        self.add_child(child)
+
+
+    def to_sql(self):
+        return "SELECT"
