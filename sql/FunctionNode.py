fcdb4253fafbfec3edbf387f8e1f05c501f5179b
Christian <cfnyu23@gmail.com>
2017-05-22 12:39:16 -0400

    SQL Node structure

---
+++ sql/FunctionNode.py
@@ -0,0 +1,18 @@
+from SQLNode import SQLNode
+from SQLNodeType import SQLNodeType
+
+
+class FunctionNode(SQLNode):
+    def __init__(self, child = None, func_type = None):
+        super(FunctionNode,self).__init__(SQLNodeType.FUNCTION_NODE, "function")
+        self.add_child(child)
+        self.func_type = func_type
+
+    def to_sql(self):
+        column_name = "*"
+        if self.child is not None and self.child.type is SQLNodeType.ATTRIBUTE_NODE:
+            column_name = self.child.to_sql()
+
+        if self.func_type is None:
+            return column_name
+        return self.func_type + "(" + column_name + ")"
